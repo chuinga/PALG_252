2e7173073614e73d67458016882d38fc1255911b
miguelMartinsDeveloper <miguel.martins@tui.com>
2025-11-13 22:09:30 +0000

    Exercicio Maior3

---
+++ Maior3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maior3
+{
+    internal class Program
+    {   // Determinar o Maior de 3 Números distintos;
+        // Exemplo
+        // 1 2 3
+        static void Main(string[] args)
+        {
+            int num1, num2, num3;
+
+            Console.Write("Digite o 1.º nr.: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite o 2.º nr.: ");
+            num2 = int.Parse(Console.ReadLine());
+            Console.Write("Digite o 3.º nr.: ");
+            num3 = int.Parse(Console.ReadLine());
+            
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine("O maior número é: " + num1);
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("O maior número é: " + num2);
+            }
+            else
+            {
+                Console.WriteLine("O maior número é: " + num3);
+            }
+
+
+
+        }
+    }
+}
