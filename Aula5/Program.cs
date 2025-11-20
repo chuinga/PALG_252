acaaa9603d7af6005b17db3c738180cfd667bc61
miguelMartinsDeveloper <miguel.martins@tui.com>
2025-11-20 19:41:24 +0000

    Aula5

---
+++ Aula5/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ciclos de Repetição – Loops
+            // While
+            // while (condition)
+            // {
+            //     statements
+            // }
+
+            int i = 0;
+
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            // Do While – Assegura que o bloco de código do loop seja executado pelo menos uma vez
+            // do
+            // {
+            //     statements
+            // } while (condition);
+
+            int j = 0;
+
+            do
+            {
+                Console.WriteLine(j);
+                j++;
+            } while (j < 10);
+
+            // For - Sabemos quantas vezes queremos repetir o bloco de código
+            // for (init; cond; incr)
+            // {
+            //     statements
+            //  }
+
+            for (int k = 1; k < 5; k++)
+            {
+                Console.WriteLine(k);
+            }
+
+            // Nested Loops
+
+            for (int h = 0; h < 3; h++)
+            {
+                Console.WriteLine($"Externo:" + h);
+
+                for (int l = 0; l < 2; l++)
+                {
+                    Console.WriteLine($"Interno:" + l);
+                }
+            }
+
+
+        }
+    }
+}
