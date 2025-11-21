2d249c154c1fd447bb4df67cd644132df44a0e15
miguelMartinsDeveloper <miguel.martins@tui.com>
2025-11-21 20:18:37 +0000

    Tabuada

---
+++ Tabuada/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+    // Dado um determinado número pelo utilizador, efetue o cálculo da tabuada desse número (1 – 10)
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tabuada de um número!");
+            Console.Write("\nIntroduza um número inteiro: ");
+            int numero;
+
+            while (!int.TryParse(Console.ReadLine(), out numero))
+            {
+                Console.Write("\nValor inválido! \nIntroduza um número inteiro: ");
+            }
+
+            Console.WriteLine($"\nTabuada do número {numero}:\n");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{numero} x {i} = {numero * i}");
+            }
+
+        }
+    }
+}
