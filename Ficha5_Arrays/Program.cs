6f28d323b85832b59d857935d8f085333c1ca6bb
miguelMartinsDeveloper <miguel.martins@tui.com>
2025-12-05 21:39:02 +0000

    Ficha 5

---
+++ Ficha5_Arrays/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Ficha5_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Exercício 1
+            // 1. Elabore um programa que leia um array com N números inteiros e indique a
+            // quantidade de elementos do array que são números negativos. 
+
+
+            #endregion
+
+            #region Exercício 2
+            // 2. Elabore um programa para ler uma sequência de 10 números inteiros positivos e
+            // calcular a soma dos números menores do que 10.
+
+
+            #endregion
+
+            #region Exercício 3
+            // 3. Elabore um programa que forme um array com as 16 primeiras letras do alfabeto.
+            // (A, B, C, D, ..., P).
+
+
+            #endregion
+
+            #region Exercício 4
+            // 4. Elabore um programa que leia um número aleatório de valores para um array de
+            // dimensão 100 e termine a digitação com 999(999 funciona como terminador da
+            // introdução de dados e determina a quantidade de dados válidos no array).O
+            // programa deverá apresentar a soma de todos os valores inseridos assim como os
+            // próprios valores.
+
+
+            #endregion
+
+            #region Exercício 5
+            // 5. Elabore um programa que leia N números para um array e calcule o somatório e a
+            // média aritmética de todos os elementos do array. 
+
+
+            #endregion
+
+            #region Exercício 6
+            // 6. Escreve um programa que leia um array de N inteiros de nome nums e mostra um
+            // array de boolean onde, cada posição indique true se o elemento da posição
+            // correspondente de nums é positivo e false caso seja negativo ou zero.
+
+
+            #endregion
+
+            #region Exercício 7
+            // 7. Escreva um programa com um array de N inteiros positivos e substitui os seus
+            // elementos de valor ímpar por - 1 e os pares por 1.
+
+
+            #endregion
+
+            #region Exercício 8
+            // 8. Elabore um programa que leia 10 valores inteiros armazenando-os num array e
+            // multiplique todos os elementos pelo maior valor armazenado. Apresente os
+            // resultados do cálculo efetuado. 
+
+
+            #endregion
+
+            #region Exercício 9
+            // 9. Elabore um programa que num vetor de N valores inteiros determine o maior e o
+            // menor.
+
+
+            #endregion
+
+        }
+    }
+}
---
+++ Ficha5_Matrizes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ficha5_Matrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Exercício 1
+            // 1. Crie uma matriz 3×3 de números inteiros, preencha-a com valores introduzidos
+            // pelo utilizador, mostre a matriz e calcule a soma de todos os elementos.
+
+
+            #endregion
+
+            #region Exercício 2
+            // 2. Crie uma matriz 4×4 preenchida com números aleatórios de 1 a 20, peça ao
+            // utilizador um número e verifique se ele existe na matriz, mostrando o resultado.
+
+
+            #endregion
+
+            #region Exercício 3
+            // 3. Crie uma matriz 3×4 com números aleatórios, mostre-a e, em seguida, crie a
+            // matriz transposta 4×3, mostrando o resultado.
+
+
+            #endregion
+
+            #region Exercício 4
+            // 4. Crie uma matriz 4×4 com números aleatórios de 1 a 50, mostre a matriz, calcule
+            // e mostre a média de cada linha e de cada coluna.
+
+
+            #endregion
+        }
+    }
+}
