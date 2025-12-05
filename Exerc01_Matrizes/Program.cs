3c1d8923dbb9bc732c5a768eabdc9cd2b57da178
miguelMartinsDeveloper <miguel.martins@tui.com>
2025-12-05 19:47:33 +0000

    Lista Presentes

---
+++ Exerc01_Matrizes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerc01_Matrizes
+{
+    internal class Program
+    {
+        /*1. Pretende-se um programa em C# capaz de ler uma matriz de 5×4 e escrever:
+            a) O maior elemento de cada linha da matriz;
+            b) A média dos elementos de cada coluna;
+            c) A soma dos elementos da linha 3;
+            d) A soma dos elementos de uma coluna escolhida pelo utilizador;
+            e) A soma dos elementos da diagonal principal;
+            f) A soma dos elementos de toda a matriz;
+            g) Os números de elementos da matriz iguais a um número dado;
+            h) A mudança de posição de todos os elementos da matriz, trocando o número da linha pela coluna. Exemplo: o elemento da linha 3 coluna 5 passa para a coluna 3 linha 5.          
+        */
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ ListaPresentes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPresentes
+{
+    internal class Program
+    {
+        // Guarde a lista de presentes de cada pessoa 
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> whislist = new Dictionary<string, List<string>>();
+
+            whislist["Patrícia"] = new List<string>() { "Memória RAM", "Game Last Of Us", "A chave do Euromilhões correta", "Livro da Agatha Christie" };
+            whislist["João"] = new List<string>() { "Consola", "Smartwatch" };
+
+            Console.WriteLine("Lista de Presentes Pretendidos:");
+
+            foreach (var item in whislist)
+            {
+
+                Console.WriteLine($"Whislist de {item.Key}");
+                foreach (var desejo in item.Value)
+                {
+                    Console.WriteLine($" - {desejo}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
